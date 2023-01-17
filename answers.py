624360d63b4e211a1eb18154ffc6a2ff56aa6450
Jennifer Esch <j.esch1@students.uu.nl>
2023-01-17 15:58:00 +0100

    Part 3 commit

--- answers.py
+++ answers.py
@@ -30,17 +30,17 @@ with open('results.json', 'w', encoding='utf-8') as file:
 
 # PART 2
 
-total_yearly_precipitation = sum(list_of_months)
+total_yearly_precipitation = sum(list_of_months) # Calculate total yearly precipitation with sum function
 print(total_yearly_precipitation)
 
-relative_monthly_precipitation_list = []
-for total_monthly_precipitation in list_of_months:
-    relative_monthly_precipitation = total_monthly_precipitation/total_yearly_precipitation
-    relative_monthly_precipitation_list.append(relative_monthly_precipitation)
+relative_monthly_precipitation_list = [] # Create a list
+for total_monthly_precipitation in list_of_months: # Iterate over the months
+    relative_monthly_precipitation = total_monthly_precipitation/total_yearly_precipitation # Calculate relative monthly precipitation
+    relative_monthly_precipitation_list.append(relative_monthly_precipitation) # Add it to the list
 
 print(relative_monthly_precipitation_list)
 
-output_data = {
+output_data = { # Add to json file
     "Seattle": {
         "station": 'GHCND:US1WAKG0038',
         "state": 'Washington state',
@@ -53,3 +53,17 @@ output_data = {
 
 with open('results.json', 'w', encoding='utf-8') as file:
     json.dump(output_data, file, indent = 4)
+
+# PART 3
+
+from csv import DictReader
+
+with open ('stations.csv') as csvfile: # Open csv file
+    stations = list(DictReader(csvfile))
+
+for station in stations: # Iterate over content in csv file
+    station_city = station['Location'] # Assign variables to columns of the csv file
+    station_state = station['State']
+    station_code = station['Station']
+
+location_precipitation = [station for station in precipitation if station['station'] == station_code]
